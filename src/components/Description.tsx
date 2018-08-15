3389825dfce1d6a1a3adbe6ec378bfa01c95ceda
werdnakof <andrewfok007@gmail.com>
2018-08-15 20:50:12 +0100

    uncomment json

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -6,7 +6,7 @@ export default class Description extends React.Component {
       <div>
         <h1>A FSM Simulator</h1>
         <h3><i>Visualize your DFAs/NFAs one symbol at a time!</i></h3>
-        <p><i>(or...you can upload a json)</i></p>
+        {/*<p><i>(or...you can upload a json)</i></p>*/}
         <p className={'code-text'}>Code available on <a href={'https://github.com/werdnakof/FSM-Visualizer'}>GitHub </a>
           and under <a href={'http://www.apache.org/licenses/LICENSE-2.0'}>Apache License v2.0</a></p>
       </div>
