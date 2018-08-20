37c8e3c70f5b53ec94db91a105e746e9f9ee4e51
werdnakof <andrewfok007@gmail.com>
2018-08-20 09:55:03 +0100

    updated description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -9,6 +9,24 @@ export default class Description extends React.Component {
         {/*<p><i>(or...you can upload a json)</i></p>*/}
         <p className={'code-text'}>Code available on <a href={'https://github.com/werdnakof/FSM-Visualizer'}>GitHub </a>
           and under <a href={'http://www.apache.org/licenses/LICENSE-2.0'}>Apache License v2.0</a></p>
+        <hr />
+        <p><b>What is a Finite State Machine (FSM) ?</b><br />
+          A Finite State Machine (aka Finite State Automaton)
+          is a mathematical model of computation which describes a definite
+          state of a system at a given point of time. The formal definition can be summarised
+          <a href={'https://en.wikipedia.org/wiki/Deterministic_finite_automaton#Formal_definition'}> here.</a>
+        </p>
+        <p>
+          <b>But...what is it used for?</b>
+          <br />
+          Designing <a href={'https://cdn-images-1.medium.com/max/1600/1*_QivJY19d1Se_4iUuQ2z2g.gif'}> Pac Man</a>!
+          <br />
+        </p>
+        <p>
+          <b>How do I use this application?</b>
+          <p>Enter your own state machine name, set of states, accepted states, start state and transitions.< br/>
+          Whenever you submit an item, the canvas area will update automatically.</p>
+        </p>
       </div>
     )
   }
